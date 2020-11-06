c44bf490924f258b7ea7d1a9af9773820df03e10
Raphael Cockx <raphael@cockx.com>
2020-11-06 17:07:59 +0100

    Updating dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import nodeResolve from "rollup-plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import replace from '@rollup/plugin-replace';
-import vue from 'rollup-plugin-vue';
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import replace from '@rollup/plugin-replace'
+import { terser } from "rollup-plugin-terser"
+import vue from 'rollup-plugin-vue'
 
 export default {
   input: "src/index.js",
@@ -24,7 +24,7 @@ export default {
   /* Cyclic dependencies are allowed in ES6, and such imports occur
      in many d3 components, so suppress those rollup warnings. */
   onwarn: function (warning, warn) {
-    if (warning.code === "CIRCULAR_DEPENDENCY") return;
-    warn(warning);
+    if (warning.code === "CIRCULAR_DEPENDENCY") return
+    warn(warning)
   }
-};
+}
